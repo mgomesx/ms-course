146e7e44accf14896c2ecaa7f4ae1a9d4f2fe3bd
unknown <marcos.gomesx@gmail.com>
2023-03-22 20:02:17 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/mgomes/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mgomes.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mgomes.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
